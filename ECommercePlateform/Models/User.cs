a0513c4baef0118cc19ffe1bebbaf670fefa27da
Tinamiyeun <Tinamiyeun@users.noreply.github.com>
2022-08-28 17:08:56 -0400

    Product and User Models added

---
+++ ECommercePlateform/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommercePlateform.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string RoleId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
