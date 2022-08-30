a46da33354364b69a158c1c3e2c20069c3665765
Tinamiyeun <Tinamiyeun@users.noreply.github.com>
2022-08-30 13:24:06 -0400

    Add images for the index page.

--- ECommercePlateform/Controllers/HomeController.cs
+++ ECommercePlateform/Controllers/HomeController.cs
@@ -22,8 +22,16 @@ namespace ECommercePlateform.Controllers
 		{
 			return View();
 		}
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
