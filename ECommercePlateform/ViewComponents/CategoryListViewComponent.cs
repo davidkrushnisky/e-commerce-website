8357c8386fca19dd98c8371365cfe42205481169
Tinamiyeun <Tinamiyeun@users.noreply.github.com>
2022-08-30 17:56:42 -0400

    Viewcomp

---
+++ ECommercePlateform/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using ECommercePlateform.Data;
+using ECommercePlateform.Enums;
+using ECommercePlateform.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ECommercePlateform.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        //private static readonly ApplicationDbContext _context = new ApplicationDbContext(DbContextOptions < ApplicationDbContext > options);
+
+        
+        public IViewComponentResult Invoke()
+        {
+            
+            return View();
+        }
+    }
+}
